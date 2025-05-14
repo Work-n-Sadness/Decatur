c640c3fc86970863d177c727b459039485192f08
Emmanuel Nkansah <emmanuelnkansah700@gmail.com>
2025-05-14 13:05:34 +0000

    "firebase-admin": "^11.0.0", "firebase-functions": "^4.0.0"

---
+++ functions/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    // "google", // You can add Google's ESLint config if desired
+    // "plugin:@typescript-eslint/recommended", // Uncomment if using TypeScript ESLint plugin
+  ],
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+  ],
+  plugins: [
+    "import",
+    // "@typescript-eslint", // Uncomment if using TypeScript ESLint plugin
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+  },
+};
